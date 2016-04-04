0424799553f5dc09a8bd54e01d62deadc17b6866
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-04 19:15:39 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,36 @@
+Role Name
+=========
+
+Install and configure sudo(1)
+
+Requirements
+------------
+
+None
+
+Role Variables
+--------------
+
+None (at the moment)
+
+Dependencies
+------------
+
+None
+
+Example Playbook
+----------------
+
+    - hosts: servers
+      roles:
+         - { sudo }
+
+License
+-------
+
+BSD
+
+Author Information
+------------------
+
+Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
---
+++ spec/serverspec/default_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe package ('sudo') do
+  it { should be_installed }
+end
+
+
+etc_dir = '/etc/sudo'
+case os[:family]
+when 'freebsd'
+  etc_dir = '/usr/local/etc'
+end
+
+describe file("#{etc_dir}/sudoers.d") do
+  it { should be_directory }
+end
+
+describe file("#{etc_dir}/sudoers.d/icinga") do
+  it { should be_file }
+  it { should be_mode 440 }
+end
+
+describe file("#{etc_dir}/sudoers") do
+  it { should be_file }
+  it { should be_mode 440 }
+end
